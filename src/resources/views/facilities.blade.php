d9055e9762a4502649145f129d7c0e93a0bae4a4
sendai-itce <itce@PC0070.local>
2019-07-18 14:22:13 +0900

    名前を変更した

--- src/resources/views/facilities.blade.php
+++ src/resources/views/facilities.blade.php
@@ -14,7 +14,7 @@
 
             <!-- タスク名 -->
             <div class="form-group">
-                <label for="fasility" class="col-sm-3 control-label">Task</label>
+                <label for="fasility" class="col-sm-3 control-label">施設名</label>
 
                 <div class="col-sm-6">
                     <input type="facility" name="name" id="facility-name" class="form-control">
@@ -25,7 +25,7 @@
             <div class="form-group">
                 <div class="col-sm-offset-3 col-sm-6">
                     <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> タスク追加
+                        <i class="fa fa-plus"></i> 追加
                     </button>
                 </div>
             </div>
@@ -36,7 +36,7 @@
     @if (count($facilities) > 0)
     <div class="panel panel-default">
         <div class="panel-heading">
-            現在のタスク
+            予約一覧
         </div>
 
         <div class="panel-body">
@@ -44,7 +44,7 @@
 
                 <!-- テーブルヘッダー -->
                 <thead>
-                    <th>Task</th>
+                    <th>施設名</th>
                     <th>&nbsp;</th>
                 </thead>
 
@@ -63,7 +63,7 @@
     {{ csrf_field() }}
     {{ method_field('DELETE') }}
 
-    <button>タスク削除</button>
+    <button>施設名削除</button>
   </form>
                             </td>
                         </tr>
