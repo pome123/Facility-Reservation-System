b4b78da42430d9a1625273e1698ce89a14af594b
sendai-itce <itce@PC0070.local>
2019-07-18 17:23:20 +0900

    h1(施設予約システム)にmarginを追加&位置を真ん中にした

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -10,11 +10,18 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+        <style>
+            .top-text {
+                text-align: center;
+                margin: 100px 0;
+            }
+        </style>
     </head>
 
     <body>
         <div>
-            <h1 class="text-center">施設予約システム</h1>
+            <h1 class="top-text">施設予約システム</h1>
             <nav>
                 <!-- ナビバーの内容 -->
             </nav>
