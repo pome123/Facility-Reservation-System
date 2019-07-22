94f4b7aa88e852586478d05625c5738f06cc28de
sendai-itce <itce@PC0070.local>
2019-07-22 13:05:08 +0900

    reservationテーブルを再びつくった

--- src/resources/views/reservations.blade.php
+++ src/resources/views/reservations.blade.php
@@ -9,24 +9,15 @@
         @include('common.errors')
 
         <!-- 新タスクフォーム -->
-        <form action="/task" method="POST" class="form-horizontal">
+        <form action="/reservation" method="POST" class="form-horizontal">
             {{ csrf_field() }}
 
             <!-- タスク名 -->
             <div class="form-group">
-                <label for="task" class="col-sm-3 control-label">Task</label>
+                <label for="reservation" class="col-sm-3 control-label">Task</label>
 
                 <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
-                </div>
-            </div>
-
-            <!-- タスク追加ボタン -->
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> タスク追加
-                    </button>
+                    <input type="text" name="name" id="reservation-name" class="form-control">
                 </div>
             </div>
         </form>
