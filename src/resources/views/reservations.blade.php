2546029ba992466a7f58e43b3d1db762df6be48f
sendai-itce <itce@PC0070.local>
2019-07-23 13:04:56 +0900

    reservation.blade.phpを作成した

--- src/resources/views/reservations.blade.php
+++ src/resources/views/reservations.blade.php
@@ -2,35 +2,22 @@
 
 @section('content')
 
-    <!-- Bootstrapの定形コード… -->
-
-    <div class="panel-body">
-        <!-- バリデーションエラーの表示 -->
-        @include('common.errors')
-
-        <!-- 新タスクフォーム -->
-        <form action="/task" method="POST" class="form-horizontal">
-            {{ csrf_field() }}
-
-            <!-- タスク名 -->
-            <div class="form-group">
-                <label for="task" class="col-sm-3 control-label">Task</label>
-
-                <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
-                </div>
+{{-- モーダル・ダイアログ --}}
+{{-- <div class="modal fade" id="addReservationModal" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span>×</span></button>
+                <h4>タイトル</h4>
             </div>
-
-            <!-- タスク追加ボタン -->
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> タスク追加
-                    </button>
-                </div>
+            <div class="modal-body">
+                本文
             </div>
-        </form>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default">予約する</button>
+            </div>
+        </div>
     </div>
+</div> --}}
 
-    <!-- TODO: 現在のタスク -->
 @endsection
