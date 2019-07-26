75439e91917f432e9c7b470f9e8d5c4d411905a2
sendai-itce <itce@PC0070.local>
2019-07-26 10:16:07 +0900

    reservationsの値を取得した データ登録の処理を書いてコメントアウトした

--- src/routes/web.php
+++ src/routes/web.php
@@ -21,31 +21,58 @@ use App\Reservation;
 Route::get('/', function () {
     // データベースから値（facilities table）を持ってくる処理を$facilitiesに代入
     $facilities = Facility::orderBy('created_at', 'asc')->get();
+    $reservations = Reservation::orderBy('created_at', 'asc')->get();
     // facilities.blade.phpに$facilitiesを渡して処理をし(facilities.blade.php)、ブラウザに返す
     return view('facilities', [
-        'facilities' => $facilities
+        'facilities' => $facilities,
+        'reservations' => $reservations,
     ]);
 });
 
 // データを登録する
-Route::post('/facilities', function (Request $request) {
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
+// Route::post('/facilities', function (Request $request) {
+//     $validator = Validator::make($request->all(), [
+//         'name' => 'required|max:255',
+//     ]);
 
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
+//     if ($validator->fails()) {
+//         return redirect('/')
+//             ->withInput()
+//             ->withErrors($validator);
+//     }
 
-    // タスク作成
-    $facility = new Facility();
-    $facility->name = $request->name;
-    $facility->save();
+//     // タスク作成
+//     $facility = new Facility();
+//     $facility->name = $request->name;
+//     $facility->save();
 
-    return redirect('/');
-});
+//     return redirect('/');
+// });
+
+// Route::post('/reservations', function (Request $request) {
+//     $validator = Validator::make($request->all(), [
+//         'date' => 'required',
+//         'period' => 'required',
+//         'facility_id' => 'required',
+//         'reservation_user' => 'required',
+//     ]);
+
+//     if ($validator->fails()) {
+//         return redirect('/')
+//             ->withInput()
+//             ->withErrors($validator);
+//     }
+
+//     // 予約一覧作成
+//     $reservation = new Reservation();
+//     $reservation->date = $request->date;
+//     $reservation->period = $request->name;
+//     $reservation->facility_id = $request->facility_id;
+//     $reservation->reservation_user = $request->reservation_user;
+//     $reservation->save();
+
+//     return redirect('/');
+// });
 
 
 // 削除ボタン
